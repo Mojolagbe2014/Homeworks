cfac7bba3b4db17fb48a6230d8002094a97af691
Mojolagbe Jamiu <mojolagbe@gmail.com>
2016-10-21 18:18:05 -0500

    Givens Rotation algorithm implemented

---
+++ 7650/givensOrthonormalize.m
@@ -0,0 +1,37 @@
+function [Q, R] = givensOrthonormalize(A)
+%% GIVENSORTHONORMALIZE Orthonormalize a matrix (set of vectors)
+%   Using Givens orthonormalization approach
+%
+%       Author: Jamiu Mojolagbe
+
+
+    [n, m] = size(A);                   % obtain the number of columns and rows of A
+    R = A;                              % start with a copy of A
+    Q = eye(n, n);                      % form an identity matrix
+    
+    for j = 1:m                         % for ach column of A
+        
+        for i = j + 1: n                % for each element below the main diagonal
+            if R(i, j) == 0
+                c = 1;
+                s = 0;
+            elseif R(j, j) == 0
+                c = 0;
+                s = R(i, j)'/abs(A(i, j));
+            else
+                c = abs(R(j, j))./sqrt((abs(R(j, j)).^2) + (abs(R(i, j)).^2));
+                s = (R(j, j) /abs(R(j, j))) * (R(i, j)' / sqrt((abs(R(j, j)).^2) + (abs(R(i, j)).^2)));
+            end
+            % Obtain Given rotation
+            givens = eye(n, n);
+            givens(i, i) = c;
+            givens(j, j) = c;
+            givens(i, j) = -s;
+            givens(j, i) = s;
+            
+            R = givens * R;         % Apply rotation 
+            Q = Q * givens';
+        end
+    end
+end
+
