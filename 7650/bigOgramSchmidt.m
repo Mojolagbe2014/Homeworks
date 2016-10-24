731b6107a8ffa7715073fb15cec1744b79e57969
Mojolagbe Jamiu <mojolagbe@gmail.com>
2016-10-23 23:44:36 -0500

    big o of gram schmidt

---
+++ 7650/bigOgramSchmidt.m
@@ -0,0 +1,32 @@
+%% bigOgramSchmidt.m
+%    Shows the big "O" complexity of Gram Schmidt Algorithm
+%        
+%       Course:     ECE 7650
+%       Homework:   1
+%       Sub. Date:  October 26, 2016
+%
+%           Author:     Jamiu Babatunde Mojolagbe
+%           Department: Electrical and Computer Engineering
+%           Student ID: #7804719
+%           Email:      mojolagm@myumanitoba.ca
+
+clear; clc; close all;
+
+%% set input variable n 
+nmax = 200;                             % maximum input n 
+nmin = 1;                               % minimum input n
+narr = linspace(nmin, nmax, nmax);      % set up input array for plotting
+
+%% calculate f(n) and cg(n)
+for n = nmin:nmax
+    f(n) = 3*(n.^3) - 2*(n.^2) + 8;
+    cg(n) = 9*(n.^3);
+end
+
+
+%% output results
+plot(narr, f, narr, cg)
+title('Big "O" complexity of Gram Schmidt Algorithm');
+xlabel('Input (n)');
+ylabel('Gram-Schmidt function');
+legend('f(n)', 'cg(n)');
