b9fd95550644ef0b992b26993c83a2d6f4bfe7ae
Mojolagbe Jamiu <mojolagbe@gmail.com>
2016-11-21 12:27:46 -0600

    properly commented

--- 7810/Homework 2/flatPlateAnalytic.m
+++ 7810/Homework 2/flatPlateAnalytic.m
@@ -1,12 +1,29 @@
+%% flatPlateAnalytic.m
+%   Calculate and demonstrate potential distribution
+%       on a flat plate with source of excitation.
+%
+%       Problem: Poisson's Equation on a Flat Plate
+%       Method:  Analytical Solution
+%        
+%       Course:     ECE 7810
+%       Homework:   2
+%       Sub. Date:  November 3, 2016
+%
+%           Author:     Jamiu Babatunde Mojolagbe
+%           Department: Electrical and Computer Engineering
+%           Student ID: #7804719
+%           Email:      mojolagm@myumanitoba.ca
+close all; clear; clc;
 
-clear; clc; close all;
-sides = [101 104 103 102];
-V1 = 100; 
-width = 1;
-height = 1;
-maxIter = 150;
-
+%% set parameters 
+sides = [101 104 103 102];                                  % line physics for the domain (probe point comes last)
+V1 = 100;                                                   % apply potential to the probe point
+width = 1;                                                  % width of the domain
+height = 1;                                                 % height of the doamin
+maxIter = 100;                                              % maximum iterations for analytic solution
 MeshData = GmshReadM('mesh_files/flat_plate.msh');          % Use GmshreadM to read the Gmsh mesh file
+
+%% solve the Poisson's problem with analytical solution
 [phi] = solveAnalytic(width, height, MeshData,V1, maxIter, sides);
 
 %% Plot the solution
