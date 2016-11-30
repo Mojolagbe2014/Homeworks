88a663da0367e513d4f978e88c0e761d16456b23
Mojolagbe Jamiu <mojolagbe@gmail.com>
2016-11-29 20:00:46 -0600

    Permutation matrix is now returned

--- 7650/Homework 3 and 4/rcm.m
+++ 7650/Homework 3 and 4/rcm.m
@@ -1,4 +1,4 @@
-function [Pi] = rcm(A)
+function [P, pi] = rcm(A)
 %% rcm.m 
 %   Implements Reverse Cuthill-McKee (RCM) Ordering of
 %       adjacency graph traversal reordering permutation 
@@ -6,14 +6,15 @@ function [Pi] = rcm(A)
 %       Parameters:
 %           A:      A sparse matrix in compressed-row format with each row sorted
 %       Returns:
-%            Pi:    A permutation list based on the ordering of the vertices traversed
+%            P:     A permutation matrix
+%            pi:    A permutation list based on the ordering of the vertices traversed
 %
 %   Author: Jamiu Babatunde Mojolagbe
 
     %% obtain the dimension of A inorder to obtain number of rows
-    [r, n, m] = getLowestDegree(A)                      % get rows and cols of A and rows of lowest degree of freedom
+    [r, n, m] = getLowestDegree(A);                     % get rows and cols of A and rows of lowest degree of freedom
     i = min(r);                                         % use the lowest row index
-    Pi = [i];                                           % permutation with starting vertex
+    pi = [i];                                           % permutation with starting vertex
     count = 1;                                          % initialize counter for the visited nodes
     marked = zeros(n, 1);                               % keep track of the visited nodes
     marked(i) = 1;                                      % mark vertex i has visited 
@@ -36,9 +37,15 @@ function [Pi] = rcm(A)
                 end   
             end
         end
-        Pi = [Pi Snew];
+        pi = [pi Snew];
         S = Snew;                                       % update level set
     end    
-    Pi = fliplr(Pi);
+    pi = fliplr(pi);
+    % obtain permutation matrix from pi
+    P = zeros(n, n);
+    for i = 1:n
+        P(i, pi(i)) = 1;
+    end
+    P = sparse(P);
 end
 
