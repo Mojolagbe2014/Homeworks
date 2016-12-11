e6525a95f50dc1b0d6bf1f6effa5ca455d365c92
Mojolagbe Jamiu <mojolagbe@gmail.com>
2016-12-10 23:00:25 -0600

    Choice of I and J initial indices changed and it gives proper u as lower triangular and v as unit upper triangular

--- 7650/Project/aca.m
+++ 7650/Project/aca.m
@@ -1,11 +1,10 @@
-function [Zh, u, v, I, J, err1] = aca(Z, tol, rk)
+function [Zh, u, v, I, J, err1] = aca(Z, tol)
 %% bfs.m 
 %   Implements the Adaptive Cross Approximation algorithm 
 %
 %       Parameters:
 %           Z:      A rank deficient/full matrix 
 %           tol:    A given error tolerance
-%           rk:     Rank of the input matrix Z
 %
 %       Returns:
 %            Zh:    Error at each iterations (Zhi hat)
@@ -23,20 +22,20 @@ function [Zh, u, v, I, J, err1] = aca(Z, tol, rk)
     I(1) = 1;                                                               % Initialize the 1st row index
     Zh = 0;
     R(I(1), :) = Z(I(1),:);                                                 % Initialize the 1st row of the approximate error matrix
-    J(1) = length(abs(R(I(1), :)));                                         % Find the 1st column index J1
+    J(1) = 1;                                                               % choose the 1st column index J1
     v = R(I(1), :)./R(I(1), J(1));
     R(:, J(1)) = Z(:, J(1));                                                % Initialize the 1st column of the approximate error matrix
     u = R(:, J(1));
     Zh = sqrt((norm(Zh,'fro').^2) + ((norm(u,'fro').^2)* (norm(v,'fro').^2)));
-    I(2) = length(abs(R(:, J(1))));                                         % Find 2nd row index I2
+    I(2) = I(1) + 1;                                                        % choose the 2nd row index I2
     
-    for k = 2:rk
+    for k = 2:min(n,m)
         sigma = 0;
         for l = 1:k-1
             sigma =sigma + u(I(k),l).*v(l, :);
         end
         R(I(k), :) = Z(I(k), :) - sigma;
-        J(k) = J(k - 1) - 1;
+        J(k) = J(k - 1) + 1;
         v(k, :) = R(I(k), :)/R(I(k), J(k));
         % Update (Jk )th column of the approximate error matrix
         sigma = 0;
@@ -52,7 +51,11 @@ function [Zh, u, v, I, J, err1] = aca(Z, tol, rk)
         Zh(k) = sqrt((norm(Zh(k-1),'fro').^2) + (2*sigma) + ((norm(u(:,k),'fro').^2)* (norm(v(k,:),'fro').^2)));
         err1(k) = (norm(u(:,k),'fro')*norm(v(k, :),'fro'));
         err2 = tol*Zh(k);
-        if  err1(k) <= err2; return; end
-        I(k+1) = I(k) - 1;
+        if  err1(k) <= err2; 
+            u = u(:, 1:k-1);
+            v = v(1:k-1, :);
+            return; 
+        end
+        I(k+1) = I(k) + 1;
     end
 end
