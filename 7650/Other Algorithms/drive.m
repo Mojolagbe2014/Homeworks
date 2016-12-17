a9830c5972a33290b21e4e6086d75ad7fb00ba68
Mojolagbe Jamiu <mojolagbe@gmail.com>
2016-12-16 22:15:46 -0600

    FOM with inner residual monitoring implemented

--- 7650/Other Algorithms/drive.m
+++ 7650/Other Algorithms/drive.m
@@ -5,15 +5,15 @@ load('data/RHS.mat');
 %% set parameters 
 A = S;
 b = RHS;
-x0 = ones(length(b), 1);
+x0 = zeros(length(b), 1);
 maxItr = 200;
-tol = 1e-12;
-m = 20;
+tol = 1e-3;
+m = 70;
 
 
 %% solve the system of equation
-
-[x, H, v] = fom(A, x0, b, m);
+[x, H, v, j] = fomrest(A, x0, b, m, tol);
+% [x, H, v] = fom(A, x0, b, m);
 % [x, itr, err] = sd(A, b, x0, maxItr, tol);
 % [x, itr, err] = rnsd(A, b, x0, maxItr, tol);              % convergence is extremely slow
 % [x, itr, err] = minRes(A, b, x0, maxItr, tol);
---
+++ 7650/Other Algorithms/fomrest.m
@@ -0,0 +1,47 @@
+function [x, H, v, j] = fomrest(A, x0, b, m, tol)
+%% fomrest.m 
+%   Implements Full Orthogonalization Method 
+%       with Inner Residual Monitoring/Tracking
+%       based on Modified-Gram Schmidt Process
+%
+%       Parameters:
+%           A:      A nxn matrix
+%           x0:     Initial guess
+%           b:      An input non-zero nx1 vector
+%           m:      Desired Krylov subspace dimension
+%           tol:    Error tolerance
+%
+%       Returns:
+%            x:    Solution vector
+%            H:    Upper Hesseberg matrix of dimension m+1xm
+%            v:    Orthonormalized vector as basis for Km
+%            j:    value of j at which it converges to the error tolerance
+%
+%   Author: Jamiu Babatunde Mojolagbe
+
+    %% set parameters
+    r = b - A*x0;
+    beta = norm(r);
+    v(:, 1) = r./beta;
+    H = zeros(m+1, m);
+
+    %% arnoldi process part based on Modified Gram Schmidt with inner residual tracking 
+    for j = 1 : m
+        w(:,j) = A*v(:, j);
+        for i = 1 : j
+            H(i,j) = dot(w(:,j), v(:, i));
+            w(:,j) = w(:,j) - H(i,j)*v(:, i);
+        end
+        H(j+1, j) = norm(w(:,j));
+        v(:, j+1) = w(:,j)./H(j+1, j);
+        % error monitoring part
+        e1 = zeros(j, 1);    e1(1) = 1;                                         % obtain ecludean basis 1
+        ej = zeros(j, 1);    ej(j) = 1;                                         % obtain ecludean basis 1
+        y = inv(H(1:j,1:j))*(beta*e1);                                          % H first m rows and columns of H bar
+        err = H(j+1, j)*(abs(dot(y, ej))./norm(b))
+        if err < tol; break; end
+    end
+    
+    %% fom additions to arnoldi process
+    x = x0 + v(:, 1:j)*y;                                                 
+end
