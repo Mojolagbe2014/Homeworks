79b108b9605ebc6c2a6364d0445040c43017c857
Mojolagbe Jamiu <mojolagbe@gmail.com>
2016-12-20 13:15:03 -0600

    Question2 completed

--- 7650/Other Algorithms/drive.m
+++ 7650/Other Algorithms/drive.m
@@ -8,7 +8,7 @@ b = RHS;
 x0 = ones(length(b), 1);
 maxItr = 40;
 tol = 1e-6;
-m = 20;
+m = 157;
 decType = 1;
 
 
@@ -25,26 +25,26 @@ decType = 1;
 % [x, itr, err] = minres(A, b, x0, maxItr, tol);
 
 
-% tol = 1e-12;
-% n = 100;                                                                                                                                                           
-% A = randn(n, n);                                                                                                                                     
-% A = 0.5*(A+A');                                                                                                                                                     
-% A = A + n*eye(n);                                                                                                                                               
-% b = randn(n, 1);                                                                                                                                                   
-% m = 100;                                                                                                                                                              
-% r = b;                                                                                                                                                               
-% beta = norm(r);                                                                                                                                                      
-% x0 = r./beta;           
+tol = 1e-12;
+n = 10;                                                                                                                                                           
+A = randn(n, n);                                                                                                                                     
+A = 0.5*(A+A');                                                                                                                                                     
+A = A + n*eye(n);                                                                                                                                               
+b = randn(n, 1);                                                                                                                                                   
+m = 5;                                                                                                                                                              
+r = b;                                                                                                                                                               
+beta = norm(r);                                                                                                                                                      
+x0 = r./beta;           
 % [x, itr, itri, err, H, v] = gmresrestart(A, x0, b, m, maxItr, tol, decType);
-x1 = A\b;
-abs(x1 - x)
-% [H, v] = arnoldi(A, x0, m);                                                                                                                                          
-% % x1 = A\b;                                                                                                                                            
-% % abs(x1 - x)                                                                                                                                         
-% Hm = H(1:m,1:m);                                                                                                                                                   
-% Vm = v(:,1:m);                                                                                                                                                     
-% Hvm = Vm*Hm*Vm';                                                                                                                                                   
-% norm(A - Hvm)      
-% 
-% Vm*Vm'
+% x1 = A\b;
+% abs(x1 - x)
+[H, v] = arnoldi(A, x0, m);                                                                                                                                          
+% x1 = A\b;                                                                                                                                            
+% abs(x1 - x)                                                                                                                                         
+Hm = H(1:m,1:m);                                                                                                                                                   
+Vm = v(:,1:m);                                                                                                                                                     
+Hvm = Vm*Hm*Vm';                                                                                                                                                   
+norm(A - Hvm)      
+
+Vm*Vm'
 % [U,R] = householder(A)
